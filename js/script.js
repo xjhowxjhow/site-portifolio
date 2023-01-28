78383590cbe27cda4d74b7a0a8c4b368abb309f1
Jhonatan Deni <jhow.1516@hotmail.com>
2023-01-27 22:34:32 -0300

    NewVersion
    :D

--- js/script.js
+++ js/script.js
@@ -1,145 +1,445 @@
 
 
-// class return element html by id
+
+
+
+
+
+
+
+// letreiros
 function getElm(elm) {
-    return document.getElementById(elm);
+  return document.getElementById(elm);
 }
 
 function getElmQuery(elm) {
-    return document.querySelector(elm);
+  return document.querySelector(elm);
 }
 
-// togle menu mobile
-const togle_menu = getElmQuery('.ghc'),
-      group_intems = getElmQuery('.header-item-2'),
-      group_intems_ul = getElmQuery('.header-item-2 ul'),
-      togle_btn = getElmQuery('.togle-menu'),
-      group_main = getElmQuery('.group-h');
+
+
+
+
+
+// function scrollpages(){
+
+//   // acessa o proximo elemento
+//   window.location.href = "#c-sobre";
+
+// }
+
+
+
+
+
+
+
+
+
+// welcome
+const textos = [
+  'Olá sou o Jhonatan Desenvolvedor Web e Desktop',
+  "Hello, I'm Jhonatan Web and Desktop Developer",
+  "Hola, soy Jhonatan Desarrollador web y de escritorio",
+  "Bonjour, je suis Jhonatan Développeur Web et de bureau",
+  "Hallo, ich bin Jhonatan Web- und Desktop-Entwickler",
+  'Olá sou o Jhonatan Desenvolvedor Web e Desktop'
+];
+
+// Armazena o elemento HTML onde o texto será exibido
+const elemento = document.getElementById("letreiro-well");
+
+// Inicia a contagem do índice do caractere a ser exibido
+let indice = 0;
+let textoAtual = 0;
+
+// Define a função que será chamada a cada intervalo de tempo
+function digitacao() {
+  // Verifica se o índice é menor que o tamanho do texto
+  if (indice <= textos[textoAtual].length) {
+    // Exibe o caractere na tela
+    elemento.innerHTML = textos[textoAtual].slice(0, indice);
+    // Incrementa o índice
+    indice++;
+
+    // Define o intervalo de tempo em que a função será chamada novamente
+    setTimeout(digitacao, 100);
+  } else {
+    // Verifica se há mais textos a serem exibidos
+    if (textoAtual < textos.length - 1) {
+      // Se houver, reinicia o índice e passa para o próximo texto
+      indice = 0;
+      textoAtual++;
+    } else {
+      // Se não houver mais textos, volta para o primeiro texto
+      textoAtual = 0;
+    }
+
+    // Inicia o intervalo de tempo novamente
+    setTimeout(digitacao, 1000);
+  }   
+}
+
+// Inicia o intervalo de tempo
+digitacao();
+
+
+// repete
+
+
+const botao1 = document.getElementById('sobre');
+const botao2 = document.getElementById('home');
+const botao3 = document.getElementById('carreira');
+const botao4 = document.getElementById('projetos');
+
+const content = document.getElementById('c-sobre');
+const content2 = document.getElementById('c-home');
+const content3 = document.getElementById('c-carreira');
+const content4 = document.getElementById('c-projetos');
+
+
+botao1.addEventListener('click', function() {
+    content.scrollIntoView({ behavior: 'smooth' });
+    botao1.style.backgroundColor = 'rgba(255, 255, 255, 0.2)'
+    botao2.style.backgroundColor = 'transparent';
+    botao3.style.backgroundColor = 'transparent';
+    botao4.style.backgroundColor = 'transparent';
+  });
+
+botao2.addEventListener('click', function() {
+    content2.scrollIntoView({ behavior: 'smooth' });
+    botao2.style.backgroundColor = 'rgba(255, 255, 255, 0.2)'
+    botao1.style.backgroundColor = 'transparent';
+    botao3.style.backgroundColor = 'transparent';
+    botao4.style.backgroundColor = 'transparent';
+  }
+);
+
+botao3.addEventListener('click', function() {
+    content3.scrollIntoView({ behavior: 'smooth' });
+    botao3.style.backgroundColor = 'rgba(255, 255, 255, 0.2)'
+    botao1.style.backgroundColor = 'transparent';
+    botao2.style.backgroundColor = 'transparent';
+    botao4.style.backgroundColor = 'transparent';
+
+  }
+);
+
+botao4.addEventListener('click', function() {
+    content4.scrollIntoView({ behavior: 'smooth' });
+    botao4.style.backgroundColor = 'rgba(255, 255, 255, 0.2)'
+    botao1.style.backgroundColor = 'transparent';
+    botao2.style.backgroundColor = 'transparent';
+    botao3.style.backgroundColor = 'transparent';
+  }
+);
+
+// headers itens
+let home = document.getElementById('home');
+let sobre = document.getElementById('sobre');
+let carreira = document.getElementById('carreira');
+let projetos = document.getElementById('projetos');
+let contato = document.getElementById('contato');
+
+function currentsectionheader(item){
+  if (item == 'c-home'){
+    home.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+    sobre.style.backgroundColor = 'transparent';
+    carreira.style.backgroundColor = 'transparent';
+    projetos.style.backgroundColor = 'transparent';
+    contato.style.backgroundColor = 'transparent';
+  }else if (item == 'txt-glosary-body'){
+    home.style.backgroundColor = 'transparent';
+    sobre.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+    carreira.style.backgroundColor = 'transparent';
+    projetos.style.backgroundColor = 'transparent';
+    contato.style.backgroundColor = 'transparent';
+  }else if (item == 'c-carreira'){
+    home.style.backgroundColor = 'transparent';
+    sobre.style.backgroundColor = 'transparent';
+    carreira.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+    projetos.style.backgroundColor = 'transparent';
+    contato.style.backgroundColor = 'transparent';
+  }else if (item == 'c-projetos'){
+    home.style.backgroundColor = 'transparent';
+    sobre.style.backgroundColor = 'transparent';
+    carreira.style.backgroundColor = 'transparent';
+    projetos.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+    contato.style.backgroundColor = 'transparent';
+  }
+  else if (item == 'c-contato'){
+    home.style.backgroundColor = 'transparent';
+    sobre.style.backgroundColor = 'transparent';
+    carreira.style.backgroundColor = 'transparent';
+    projetos.style.backgroundColor = 'transparent';
+    contato.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+  }
+}
+
+// TIME LINE
+
+
+const animatedElements = document.querySelectorAll('.menu-item, .txt-glosary-body, .timeline__event, .txt-glosary-footer, .title-page-project, .img-2');
+
+animatedElements.forEach(animatedElement => {
+
+const observer = new IntersectionObserver(entries => {
+
+    if (entries[0].intersectionRatio > 0) {
+
+      if (animatedElement.classList.contains('timeline__event')) {
+
+        let delay = 0;
+        let elementos = document.querySelectorAll('.timeline__event');
+        let effects = ['fadeInLeft', 'fadeInRight','fadeinTopLeft', 'fadeInUp','fadeInDown'];
+        
+
+        elementos.forEach(elemento => {
+          let randomIndex = Math.floor(Math.random() * effects.length);
+
+          let randomElement = effects[randomIndex];
+          elemento.classList.add('animated',randomElement , 'delay-' + delay + 's');
+          delay += 1;
+        });
+
+
+      }
+      if (animatedElement.classList.contains('menu-item')) {
+
+
+        let elementos = document.querySelectorAll('.menu-item');
+        let delay = 0;
+
+        elementos.forEach(elemento => {
+          elemento.classList.add('animated', 'fadeInUp', 'delay-' + delay + 's');
+          delay += 1;
+        });
+      }
+      if (animatedElement.classList.contains('txt-glosary-body')) {
+
+
+        animatedElement.classList.add('animated', 'fadeInLeft', 'delay-1s');
+        // muda o item no header
+
+      }
+      if (animatedElement.classList.contains('txt-glosary-footer')) {
+
+
+        animatedElement.classList.add('animated', 'fadeInRight', 'delay-1s');
+      }
+      if (animatedElement.classList.contains('title-page-project')) {
+
+        
+        animatedElement.classList.add('animated', 'fadeInLeft', 'delay-1s');
+
+      }
       
-// click event 
-let togle_s = 0;
-getElmQuery('.togle-menu').onclick = function() {
-    // expand menu
-    if (togle_s == 0) {
+      if (animatedElement.classList.contains('img-2')) {
 
-        togle_menu.style.height = '100%';
-        group_intems.style.display = 'block';
-        group_intems_ul.style = 'flex-direction: column; display: flex; justify-content: center; align-items: center;';
-        togle_btn.style.transform = 'rotate(180deg)';
-        togle_btn.style.transition = 'all 0.5s';
-        togle_s = 1;
-        }
-    // close menu
-    else {
-        togle_menu.style.height = '50px';
-        group_intems_ul.style = 'flex-direction: row';
-        group_intems.style.display = 'none';
-        togle_btn.style.transform = 'rotate(0deg)';
-        togle_s = 0;
-        }
-}
+        
+        animatedElement.classList.add('animated', 'fadeInRight', 'delay-1s');
+      }
+      
 
-// event resize
-window.onresize = function() {
-    if (window.innerWidth > 768) {
-        togle_menu.style.height = '50px';
-        group_intems.style.display = 'block';
-        group_intems_ul.style = 'flex-direction: row';
-        togle_btn.style.transform = 'rotate(0deg)';
-        togle_s = 0;
+      observer.disconnect();
     }
-    else {
-        togle_menu.style.height = '50px';
-        group_intems.style.display = 'none';
-        group_intems_ul.style = 'flex-direction: row';
-        togle_btn.style.transform = 'rotate(0deg)';
-        togle_s = 0;
+  });
+
+
+  observer.observe(animatedElement);
+});
+
+// Seleciona as divs que deseja observar
+
+// Cria um novo observador de interseção
+const sections = document.querySelectorAll("#c-home, #c-sobre, #c-carreira, #c-projetos, #c-contato");
+
+window.addEventListener("scroll", function() {
+  sections.forEach(function(section) {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
+    if (sectionTop <= 0 && sectionBottom > 0) {
+
+      currentsectionheader(section.id);
     }
-}
+  });
+});
+
+
+
+
 
 let default_slide = 0;
+let qt_slide = 5;
 
-// get button next and prev and add event listener
-
-const next = document.querySelector('.next');
-
-const prev = document.querySelector('.prev');
-// --selected-item: 1; css variable
-const value = getComputedStyle(document.documentElement).getPropertyValue('--selected-item'); 
-
-
-// get all slides
-const query = document.querySelectorAll('.slides-list');
-
-// quantidade de slides
-let qt_slide = 4;
-
+//SLIDE PROJECTS
 function slides_n_p(click_id) {
-// #function left: calc(var(--selected-item) * -100%);
-    if (click_id == 'next') {
-        if (default_slide ==0 ) {
-
-            document.documentElement.style.setProperty('--selected-item', default_slide + 1);
-            default_slide += 1;
-            }
-        else if (default_slide == qt_slide){
-            default_slide = 0;
-            document.documentElement.style.setProperty('--selected-item', default_slide);
-            }
-        else {
-            document.documentElement.style.setProperty('--selected-item', default_slide + 1);
-            console.log(value);
-            default_slide += 1;
-            }
-    }
-    else if (click_id == 'previus') {
-        if (default_slide == 0) {
-            default_slide = qt_slide;
-            document.documentElement.style.setProperty('--selected-item', default_slide);
-            }
-        else {
-            document.documentElement.style.setProperty('--selected-item', default_slide - 1);
-            default_slide -= 1;
-            }
-        
-    }
-    else {
-        alert('error');
-    }
-
-}
-
+  // #function left: calc(var(--selected-item) * -100%);
+      if (click_id == 'next') {
+          if (default_slide ==0 ) {
+  
+              document.documentElement.style.setProperty('--selected-item', default_slide + 1);
+              default_slide += 1;
+              }
+          else if (default_slide == qt_slide){
+              default_slide = 0;
+              document.documentElement.style.setProperty('--selected-item', default_slide);
+              }
+          else {
+              document.documentElement.style.setProperty('--selected-item', default_slide + 1);
+              
+              default_slide += 1;
+              }
+      }
+      else if (click_id == 'previus') {
+          if (default_slide == 0) {
+              default_slide = qt_slide;
+              document.documentElement.style.setProperty('--selected-item', default_slide);
+              }
+          else {
+              document.documentElement.style.setProperty('--selected-item', default_slide - 1);
+              default_slide -= 1;
+              }
+          
+      }
+      else {
+          alert('error');
+      }
+  
+  }
+  
 var nome_variavel = setInterval(slide_timer, 5000);
-
+  
 function slide_timer() {
-    const value = default_slide;
-    if (value == 0) {
-        document.documentElement.style.setProperty('--selected-item', 1);
-        default_slide = 1;
-        }
-    else if (value == qt_slide) {
-        document.documentElement.style.setProperty('--selected-item', 0);
-        default_slide = 0;
-
-        }
-    else {
-        document.documentElement.style.setProperty('--selected-item', value + 1);
-        default_slide += 1;
-
-        }
+      const value = default_slide;
+      if (value == 0) {
+          document.documentElement.style.setProperty('--selected-item', 1);
+          default_slide = 1;
+          }
+      else if (value == qt_slide) {
+          document.documentElement.style.setProperty('--selected-item', 0);
+          default_slide = 0;
+  
+          }
+      else {
+          document.documentElement.style.setProperty('--selected-item', value + 1);
+          default_slide += 1;
+  
+          }
 }
-
-
+  
+  
 // evento pause timer
 
-const targetCircle = getElm('content-slide');
+const targetCircle = getElm('c-projetos');
 
 targetCircle.onmouseover = function() {
     clearInterval(nome_variavel);
-    console.log('pause');
+
 }
 
 targetCircle.onmouseout = function() {
     nome_variavel = setInterval(slide_timer, 5000);
-    console.log('play');
+
 }
+
+
+
+
+// toggle menu mobile
+// togle menu mobile
+const togle_menu = getElmQuery('.togle-menu'),
+      header = getElmQuery('.header'),
+      group_menu = getElmQuery('.menu'),
+      logo_text = getElmQuery('.logo-text'),
+      menu_item = getElmQuery('.menu-item')
+      
+    
+let togle_s = 0;
+
+getElmQuery('.togle-menu').onclick = function() {
+  // expand menu
+  if (togle_s == 0) {
+    header.style = 'height:100vh; background-color: rgb(40, 42, 54);'
+    group_menu.style = 'display: flex; flex-direction: column; justify-content: center; align-items: center;';
+    togle_s = 1;
+    togle_menu.style.transform = 'rotate(180deg)';
+    console.log('togle expand')
+    logo_text.style = 'width: 20%;'
+    menu_item.style= 'font-size: 2rem;'
+    for (let i = 0; i < group_menu.children.length; i++) {
+      group_menu.children[i].style = 'font-size: 1.2rem;';
+    } 
+    
+  }
+
+  // close menu
+  else {
+
+    header.style = '20px ;background-color: rgba(92, 29, 165, 0.2);';
+    togle_menu.style.transform = 'rotate(0deg)';
+    group_menu.style = 'display: flex; flex-direction: row; justify-content: space-between; align-items: center;';
+    togle_s = 0;
+    group_menu.style= 'display: none;'
+  }
+}
+
+
+
+window.onresize = function() {
+  if (window.innerWidth > 768) {
+      togle_menu.style.height = '20px';
+      togle_menu.style.transform = 'rotate(180deg)';
+      togle_s = 0;
+      group_menu.style= 'display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin: 0; padding: 0; gap: 10px; margin-right: 20px;'
+      header.style = 'width: 100%; position: fixed; justify-content: space-between; align-items: center; padding: 20px 0px; z-index: 100; transition: 0.2s;';
+      logo_text.style = 'width: 100%;'
+      menu_item.style= 'font-size: 1rem;'
+      for (let i = 0; i < group_menu.children.length; i++) {
+        group_menu.children[i].style = 'font-size: 1rem;';
+      }
+      
+      
+  }
+  else {
+      togle_menu.style.height = '50px';
+      togle_menu.style.transform = 'rotate(0deg)';
+      togle_s = 0;
+      group_menu.style= 'display: none;'
+      logo_text.style = 'width: 100%;'
+      menu_item.style= 'font-size: 1rem;'
+      for (let i = 0; i < group_menu.children.length; i++) {
+        group_menu.children[i].style = 'font-size: 1rem;';
+      }
+
+  }
+}
+
+// verifica ao abrir o site se o tamanho da tela é maior que 768px
+
+// if (window.innerWidth > 768) {
+//   togle_menu.style.height = '20px';
+//   togle_menu.style.transform = 'rotate(0deg)';
+//   togle_s = 0;
+//   group_menu.style= 'display: flex; flex-direction: row; justify-content: space-between; align-items: center; margin: 0; padding: 0; gap: 10px; margin-right: 20px;'
+//   header.style = 'width: 100%; position: fixed; justify-content: space-between; align-items: center; padding: 20px 0px; z-index: 100; transition: 0.2s;';
+//   logo_text.style = 'width: 100%;'
+//   menu_item.style= 'font-size: 1rem;'
+//   for (let i = 0; i < group_menu.children.length; i++) {
+//     group_menu.children[i].style = 'font-size: 1rem;';
+//   }
+  
+   
+// }
+// else {
+//   togle_menu.style.height = '20px';
+//   togle_menu.style.transform = 'rotate(0deg)';
+//   togle_s = 0;
+//   group_menu.style= 'display: none;'
+//   logo_text.style = 'width: 100%;'
+//   menu_item.style= 'font-size: 1rem;'
+//   for (let i = 0; i < group_menu.children.length; i++) {
+//     group_menu.children[i].style = 'font-size: 1rem;';
+//   }
+  
+// }
